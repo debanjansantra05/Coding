b8c89eaf212081adf066085ff5e964538f38cff5
debanjansantra05 <debanjansantra2014@gmail.com>
2026-06-14 20:58:16 +0530

    3Sum

---
+++ Arrays/LeetCode/15. 3Sum.py
@@ -0,0 +1,39 @@
+#3Sum
+
+n = int(input())
+nums = list(map(int, input().split()))
+
+nums.sort()
+result = []
+
+for i in range(n - 2):
+    if i > 0 and nums[i] == nums[i - 1]:
+        continue
+
+    left = i + 1
+    right = n - 1
+
+    while left < right:
+        total = nums[i] + nums[left] + nums[right]
+
+        if total == 0:
+            result.append([nums[i], nums[left], nums[right]])
+
+            while left < right and nums[left] == nums[left + 1]:
+                left += 1
+
+            while left < right and nums[right] == nums[right - 1]:
+                right -= 1
+
+            left += 1
+            right -= 1
+
+        elif total < 0:
+            left += 1
+        else:
+            right -= 1
+
+if len(result) == 0:
+    print("[]")
+else:
+    print(result)
